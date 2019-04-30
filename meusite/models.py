e9fccf430c57a6f90032d8a2d5cb257b7a7b5729
Witzke <jubrito@usp.br>
2019-04-30 11:55:10 -0300

    Criar Models

--- meusite/models.py
+++ meusite/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Candidato(models.Model):
+    genero_feminino = 'f'
+    genero_masculino = 'm'
+    genero_outro = 'o'
+
+    genero_opcoes = [
+        (genero_feminino, 'Feminino'),
+        (genero_masculino, 'Masculino'),
+        (genero_outro, 'Outro'),
+    ]
+
+    nome = models.CharField(max_length=100)
+    idade = models.DateField()
+    genero = models.CharField(max_length=2, choices=genero_opcoes)
+    dt_nascimento = models.DateField()
+    nacionalidade = models.CharField(max_length=50)
+    jatrabalha = models.BooleanField(default=True)
+    pretencao_salarial = models.DecimalField(decimal_places=2, max_digits=1000, default=0)
+    perfil = models.TextField()
--- meusite/static/script.js
+++ meusite/static/script.js
@@ -7,17 +7,25 @@ let botaoProx = document.querySelector(".proximo");
 let botao = document.querySelector(".pop_up");
 let popup = document.querySelector(".artes");
 let imgFront = document.querySelector(".img_front");
+let imgArte = document.querySelector(".arte");
 let descricaoImgFront = document.querySelector(".texto_portfolio");
+let descricaoArte = document.querySelector(".texto_arte");
 let corpo = document.querySelector("body");
 let menuHamburguer = document.querySelector(".menu_hamburguer");
 let menuNav = document.querySelector(".menu_navegacao");
 let traco = document.querySelector(".traco");
-let menuX = document.querySelector(".menu_X")
+let menuX = document.querySelector(".menu_X");
 
 let listaFront=[
     /* 0 */{url:'../static/imagens/front/1.png', tag: "Texto do Site 1"},
     /* 1 */{url:'../static/imagens/front/2.png', tag: "Texto do Site 2"},
     /* 2 */{url:'../static/imagens/front/3.png', tag: "Texto do Site 3"}
+]
+
+let listaDesign=[
+    /* 0 */{url:'../static/imagens/design/1.png', tag: "Descrição Arte 1"},
+    /* 1 */{url:'../static/imagens/design/2.png', tag: "Descrição Arte 2"},
+    /* 2 */{url:'../static/imagens/design/3.png', tag: "Descrição Arte 3"}
 ]
 
 function frontIterativo(){
@@ -27,13 +35,16 @@ function frontIterativo(){
     if (i==listaFront.length) i=0;
 }
 
+function designIterativo(){
+    imgArte.src = listaDesign[i].url;
+    descricaoArte.innerHTML = listaDesign[i].tag;
+    i++;
+    if (i==listaDesign.length) i=0;
+    popup.classList.toggle("aparecerPopUp");
+}
+
 botaoProx.onclick = frontIterativo;
 corpo.classList.add("botaoFront");/*adicionar/remover classes existentes no CSS*/
-
-function abrirPopUp(){
-    popup.classList.toggle("aparecerPopUp");
-    
-}
 
 function abrirMenuHamburguer(){
     menuNav.classList.toggle("aparecerMenuNavegacao");
@@ -41,6 +52,6 @@ function abrirMenuHamburguer(){
     menuX.classList.toggle("aparecer_menu_x");   
 }
 
-botao.onclick = abrirPopUp;
+botao.onclick = designIterativo;
 menuHamburguer.onclick = abrirMenuHamburguer;
 menuX.onclick = abrirMenuHamburguer;
